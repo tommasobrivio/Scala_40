a34662e3adc461a11abfac31f02a31a2e0c8b0d8
tommasobrivio <tommasobrivio150@gmail.com>
2023-11-13 11:25:15 +0100

    third commit

---
+++ scala40_Client/src/Carta.java
@@ -0,0 +1,12 @@
+public class Carta {
+    
+    public char cover;
+    public char rank;
+    public char type;
+    public int value;
+
+    public Carta(){
+
+    }
+
+}
--- scala40_Client/src/User.java
+++ scala40_Client/src/User.java
@@ -1,6 +1,9 @@
+import java.util.List;
+
 public class User {
     public String nome;
     public boolean pronto;
+    public List<Carta> carteMano;
 
     public User(String s){
         nome=s;
